faed4b594e42af208c24544822e160687a50f305
Alex Pyankov <pyankov.work@gmail.com>
2018-08-12 09:29:26 +0300

    Add Web-options class

---
+++ TicTacToeLibrary/Options.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeLibrary
+{
+    /// <summary>
+    /// Класс, хранящий в себе основные сетевые настройки
+    /// </summary>
+    public static class Options
+    {
+        /// <summary>
+        /// Номер порта
+        /// </summary>
+        public static int Port = 8888;
+        /// <summary>
+        /// Ip-адрес сервера
+        /// </summary>
+        public static string Addres = "127.0.0.1";
+    }
+}
