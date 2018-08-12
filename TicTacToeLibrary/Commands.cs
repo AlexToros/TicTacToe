f507cac025b462256f88abcb33b7de8d1560b6a9
Alex Pyankov <pyankov.work@gmail.com>
2018-08-12 09:01:57 +0300

    Add libriary for Client-Server interactions

---
+++ TicTacToeLibrary/Commands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeLibrary
+{
+    /// <summary>
+    /// Перечисление комманд, доступных для передачи серверу
+    /// </summary>
+    public enum Commands
+    {
+        /// <summary>
+        /// Приглашение другого игрока
+        /// </summary>
+        INVITE = 0x001,
+    }
+}
---
+++ TicTacToeServer/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace TicTacToeServer
+{
+    class Player
+    {
+        private TcpClient client;
+        public Player(TcpClient Client)
+        {
+            client = Client;
+        }
+
+        public void Process()
+        {
+            NetworkStream stream = client.GetStream();
+        }
+    }
+}
